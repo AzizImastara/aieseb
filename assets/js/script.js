0ae91fdfaac0b3f6fff254772554c2ce8d6ec240
Elan34W <elanagumw@gmail.com>
2024-04-22 09:25:40 +0700

    new home style js img

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,48 @@ document.getElementById("logo").addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 3000);
+}
+
+const slides = document.querySelectorAll(".slides .slide");
+let slideIndex = 0;
+let intervalId = null;
+
+
+function showSlide(index) {
+  if (index >= slides.length) {
+      slideIndex = 0;
+  } else if (index < 0) {
+      slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+      slide.classList.remove("displaySlide");
+  });
+
+  const currentSlide = slides[slideIndex];
+  currentSlide.classList.add("displaySlide");
+}
+
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  if (slideIndex < 0) {
+      slideIndex = slides.length - 1;
+  }
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+      slideIndex = 0;
+  }
+  showSlide(slideIndex);
+}
+
